39d3636ade98e4ecf96056ccd17415176476f7b1
unknown <admin@CHETUIWK217.chetu.com>
2022-03-15 17:46:47 +0530

    Crud project

---
+++ MVC2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using MVC2.DB_Context_EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            manojEntities obj = new manojEntities();
+            var res = obj.carmodels.ToList();
+            List<carmodel> carobj = new List<carmodel>();
+            
+           foreach(var item in res)
+            {
+                carobj.Add(new carmodel
+                {
+                    id = item.id,
+                    name = item.name,
+                    model = item.model,
+                    price = item.price
+                });
+            }
+           
+            return View(res);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC2/Models/emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2.Models
+{
+    public class emp
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string model { get; set; }
+        public int price { get; set; }
+    }
+}
